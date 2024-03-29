182435cea7cb38fd1a30fb57b73dda41dcbf343d
Your Name <you@example.com>
2024-03-30 01:24:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,106 @@
+// 1-masala
+
+// let arr = [1,2,3,4,5]
+// let box = arr.pop()
+// let item = arr.shift()
+// console.log(box + item)
+
+
+
+// // 2-masala
+// let arr = parseInt(Math.random()* 100)
+// if( arr % 2 == 0 ){
+//     console.log(arr + "random soni juft");
+//     console.log(arr + 1 + "random soni toq");
+// }else{
+//     console.log(arr + "random soni juft");
+//     console.log(arr + 1 + "random soni toq");
+// }
+
+
+
+// // 3-masala
+
+
+// let arr = [5, 16, 7, 4, 10, 3, 4, 4, 14, 5, 14, 4, 44, 4]
+// let total = []
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] != 4){
+//         total.push(arr[i])
+//     }
+// }
+// console.log(total);
+
+
+// // 4-masala
+
+// let arr = [1,3,2,4,5,6,99,102]
+// arr.sort((a, b)=> a - b)
+// for(let i = 0; i < arr.Length; i++){
+//    if(arr[i] == 5){
+//         console.log("usish tartibida " + i + "ga teng");
+//     }
+// }
+// arr.sort((a, b)=> b - a)
+// for(let i = 0; i < arr.Length; i++){
+//     if(arr[i] == 5){
+//         console.log("kamayish  tartibida " + i + "ga teng");
+//     }
+// }    
+
+
+// 5-masala
+
+// 1-usul
+
+// let arr = [1,2,3,4,5,6,7,8,89]
+// let item = 0
+// for (let i = 0; i < arr.length; i++) {
+//     item += arr[i]
+// }
+// console.log(item)
+
+// 2-usul
+
+// let arr = [1,2,3,4,5,6,7,8,89]
+// let item = arr.reduce((a,b)=> a + b)
+// console.log(item)
+
+
+
+
+// 6-masala
+
+// function fn(target){
+// let arr =  [1,3,2,4,5,6,99,102]
+// arr.sort((a, b)=> a-b)
+// console.log(arr)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == target){
+//         console.log("Osish tartibida " + i + "ga teng");
+//     }
+// }
+// arr.sort((a, b)=> b - a)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == target){
+//         console.log("Kamayish tartibida " + i + "ga teng");
+//     }
+// }
+// }
+// fn(3)
+// fn(102)
+
+
+// 7-masala
+
+
+
+
+
+// 8-masala
+
+// let obj = {old: 19, school: 35}
+// function fn(target){
+    
+// }
+// fn()
